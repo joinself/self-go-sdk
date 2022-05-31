44555d36c109d0859c57ff58dd888c28dbb69813
Adrià Cidre <593270+adriacidre@users.noreply.github.com>
2022-05-31 12:58:31 +0200

    fix review comments

--- authentication/client.go
+++ authentication/client.go
@@ -8,9 +8,18 @@ import (
 	"github.com/joinself/self-go-sdk/fact"
 )
 
+type Requester interface {
+	Request(req *fact.FactRequest) (*fact.FactResponse, error)
+	RequestAsync(req *fact.FactRequestAsync) error
+	GenerateQRCode(req *fact.QRFactRequest) ([]byte, error)
+	GenerateDeepLink(req *fact.DeepLinkFactRequest) (string, error)
+	WaitForResponse(cid string, exp time.Duration) (*fact.QRFactResponse, error)
+	Subscribe(auth bool, sub func(sender string, res *fact.StandardResponse))
+}
+
 // Service handles all fact operations
 type Service struct {
-	requester fact.Service
+	requester Requester
 	expiry    time.Duration
 }
 
