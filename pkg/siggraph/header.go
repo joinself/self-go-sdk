344d33ba2350c8c41703bf1cdc36fa0f971d7b18
Tom Bevan <tom.bevan@r3labs.io>
2024-04-19 16:46:24 +0400

    cleanup legacy

--- pkg/siggraph/header.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2020 Self Group Ltd. All Rights Reserved.
-
-package siggraph
-
-// Header represents a jws header
-type Header struct {
-	Algorithm string `json:"alg"`
-	KeyID     string `json:"kid"`
-}
