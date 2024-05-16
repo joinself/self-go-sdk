5b298ea54678890385dad18f15de619f6f54cdd6
Tom Bevan <tom@joinself.com>
2024-05-17 00:10:00 +0400

    verifiable presentation tests

--- account/account_test.go
+++ account/account_test.go
@@ -138,7 +138,7 @@ func TestAccountMessaging(t *testing.T) {
 		Create().
 		Identifier(identityKey).
 		GrantEmbedded(invocationKey, identity.RoleInvocation).
-		GrantEmbedded(multiroleKey, identity.RoleAuthentication|identity.RoleMessaging).
+		GrantEmbedded(multiroleKey, identity.RoleVerification|identity.RoleAuthentication|identity.RoleMessaging).
 		SignWith(identityKey).
 		SignWith(invocationKey).
 		SignWith(multiroleKey).
@@ -199,9 +199,11 @@ func TestAccountIdentity(t *testing.T) {
 	document, err = alice.IdentityResolve(identityKey)
 	require.Nil(t, err)
 
+	// we have removed the authentication role, but this won't be reflected immediately upon
+	// querying, as we recently queried for this identity.
 	assert.True(t, document.HasRolesAt(multiroleKey, identity.RoleVerification, time.Now()))
 	assert.True(t, document.HasRolesAt(multiroleKey, identity.RoleMessaging, time.Now()))
-	assert.False(t, document.HasRolesAt(multiroleKey, identity.RoleAuthentication, time.Now()))
+	assert.True(t, document.HasRolesAt(multiroleKey, identity.RoleAuthentication, time.Now()))
 }
 
 func TestAccountCredentials(t *testing.T) {
@@ -226,6 +228,14 @@ func TestAccountCredentials(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, bobbyIdentifiers.Length(), 1)
 
+	bobbyKeys, err := bobby.KeychainSigningAssociatedWith(
+		bobbyIdentifiers.Get(0),
+		identity.RoleAssertion,
+	)
+	require.Nil(t, err)
+	require.Equal(t, bobbyKeys.Length(), 1)
+
+	// generate a credential for bobby, issued by alice
 	passportCredential, err := credential.NewCredential().
 		CredentialType(credential.CredentialPassport).
 		CredentialSubject(credential.AddressAure(bobbyIdentifiers.Get(0))).
@@ -251,6 +261,34 @@ func TestAccountCredentials(t *testing.T) {
 	err = passportVerifiableCredential.Validate()
 	require.Nil(t, err)
 
+	// store the credential on bobbys account
 	err = bobby.CredentialStore(passportVerifiableCredential)
 	require.Nil(t, err)
+
+	// retrieve the credential from bobbys account
+	verifiableCredentials, err := bobby.CredentialLookupByCredentialType(credential.CredentialPassport)
+	require.Nil(t, err)
+	require.Equal(t, 1, verifiableCredentials.Length())
+
+	passportPresentation, err := credential.NewPresentation().
+		Presentationtype(credential.PresentationPassport).
+		Holder(
+			credential.AddressAureWithKey(
+				bobbyIdentifiers.Get(0),
+				bobbyKeys.Get(0),
+			),
+		).
+		CredentialAdd(verifiableCredentials.Get(0)).
+		Finish()
+
+	require.Nil(t, err)
+
+	passportVerifiablePresentation, err := bobby.PresentationIssue(passportPresentation)
+	require.Nil(t, err)
+
+	err = passportVerifiablePresentation.Validate()
+	require.Nil(t, err)
+
+	credentials := passportVerifiablePresentation.Credentials()
+	assert.Equal(t, 1, credentials.Length())
 }
