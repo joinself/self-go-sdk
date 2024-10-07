d2dbd8eaa0262894f58aa29c5f290352a658b701
Tom Bevan <tom@joinself.com>
2024-10-07 15:50:22 +0400

    allow configurable account environment targets

--- account/config.go
+++ account/config.go
@@ -13,29 +13,46 @@ import "github.com/joinself/self-go-sdk-next/message"
 type LogLevel uint32
 
 const (
-	LogError      LogLevel = C.LOG_ERROR
-	LogWarn       LogLevel = C.LOG_WARN
-	LogInfo       LogLevel = C.LOG_INFO
-	LogDebug      LogLevel = C.LOG_DEBUG
-	LogTrace      LogLevel = C.LOG_TRACE
-	TargetDevelop          = "develop"
-	TargetSandbox          = "sandbox"
+	LogError LogLevel = C.LOG_ERROR
+	LogWarn  LogLevel = C.LOG_WARN
+	LogInfo  LogLevel = C.LOG_INFO
+	LogDebug LogLevel = C.LOG_DEBUG
+	LogTrace LogLevel = C.LOG_TRACE
 )
 
 var (
-	defaultRpcDevelop     = "http://127.0.0.1:8080/"
-	defaultObjectDevelop  = "http://127.0.0.1:8090/"
-	defaultMessageDevelop = "ws://127.0.0.1:9000/"
-	defaultRpcSandbox     = "https://rpc.sandbox.joinself.com/"
-	defaultObjectSandbox  = "https://object.sandbox.joinself.com/"
-	defaultMessageSandbox = "wss://message.sandbox.joinself.com/"
+	TargetProduction = &Target{
+		Rpc:     defaultRpcProduction,
+		Object:  defaultObjectProduction,
+		Message: defaultMessageProduction,
+	}
+	TargetSandbox = &Target{
+		Rpc:     defaultRpcSandbox,
+		Object:  defaultObjectSandbox,
+		Message: defaultMessageSandbox,
+	}
+)
+
+type Target struct {
+	Rpc     string
+	Object  string
+	Message string
+}
+
+var (
+	defaultRpcSandbox        = "https://rpc.sandbox.joinself.com/"
+	defaultObjectSandbox     = "https://object.sandbox.joinself.com/"
+	defaultMessageSandbox    = "wss://message.sandbox.joinself.com/"
+	defaultRpcProduction     = "https://rpc.joinself.com/"
+	defaultObjectProduction  = "https://object.joinself.com/"
+	defaultMessageProduction = "wss://message.joinself.com/"
 )
 
 // Config stores config for an account
 type Config struct {
 	StorageKey  []byte
 	StoragePath string
-	Environment string
+	Environment *Target
 	LogLevel    LogLevel
 	Callbacks   Callbacks
 }
@@ -56,34 +73,7 @@ func (c *Config) defaults() {
 		c.LogLevel = LogError
 	}
 
-	if c.Environment != TargetDevelop && c.Environment != TargetSandbox {
+	if c.Environment != TargetProduction && c.Environment != TargetSandbox {
 		c.Environment = TargetSandbox
-	}
-}
-
-func (c *Config) rpcURL() string {
-	switch c.Environment {
-	case TargetDevelop:
-		return defaultRpcDevelop
-	default:
-		return defaultRpcSandbox
-	}
-}
-
-func (c *Config) objectURL() string {
-	switch c.Environment {
-	case TargetDevelop:
-		return defaultObjectDevelop
-	default:
-		return defaultObjectSandbox
-	}
-}
-
-func (c *Config) messageURL() string {
-	switch c.Environment {
-	case TargetDevelop:
-		return defaultMessageDevelop
-	default:
-		return defaultMessageSandbox
 	}
 }
