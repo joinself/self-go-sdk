5f1ef13035c767c4466ca5ffa405236e530ec96a
Tom Bevan <tom@joinself.com>
2025-06-17 20:01:45 +0400

    update examples

--- examples/agreement/main.go
+++ examples/agreement/main.go
@@ -4,16 +4,15 @@ import (
 	"bytes"
 	"encoding/hex"
 	"fmt"
+	"log"
 	"os"
 	"sync"
 	"time"
 
-	"github.com/charmbracelet/log"
 	"github.com/go-pdf/fpdf"
 	"github.com/joinself/self-go-sdk/account"
 	"github.com/joinself/self-go-sdk/credential"
 	"github.com/joinself/self-go-sdk/event"
-	"github.com/joinself/self-go-sdk/keypair/signing"
 	"github.com/joinself/self-go-sdk/message"
 	"github.com/joinself/self-go-sdk/object"
 )
@@ -21,188 +20,53 @@ import (
 var requests sync.Map
 
 func main() {
-	// initialize an account that will be used to interact with self and other entities on
-	// the network. the account provides storage of all cryptographic key material, as well
-	// as credentials and all state used for e2e encrypted messaging with other entitites
 	cfg := &account.Config{
-		// provide a secure storage key that will  be used to encrypt your local account
-		// state. this should be replaced with a securely generated key!
-		StorageKey: make([]byte, 32),
-		// provide a storage path to the directory where your local account state will be
-		// stored
+		StorageKey:  []byte("my secure random key"),
 		StoragePath: "./storage",
-		// provide an environment to target [Develop, Sandbox]
 		Environment: account.TargetSandbox,
-		// provide the level of log granularity [Error, Warn, Info, Debug, Trace]
-		LogLevel: account.LogWarn,
-		// specify callbacks to handle events
+		LogLevel:    account.LogWarn,
 		Callbacks: account.Callbacks{
-			// invoked when the messaging socket connects
-			OnConnect: func(selfAccount *account.Account) {
-				log.Info("messaging socket connected")
-			},
-			// invoked when the messaging socket disconnects. if there is no error
-			OnDisconnect: func(selfAccount *account.Account, err error) {
-				if err != nil {
-					log.Warn("messaging socket disconnected", "error", err)
-				} else {
-					log.Info("messaging socket disconnected")
-				}
-			},
-			// invoked when there is a response to a discovery request from a new address.
-			OnWelcome: func(selfAccount *account.Account, wlc *event.Welcome) {
-				// we have received a response to our discovery request that is from a new
-				// user/address that we do not have an  end to end encrypted session.
-				// accept the invite to join the encrypted group created by the user.
-				groupAddress, err := selfAccount.ConnectionAccept(
-					wlc.ToAddress(),
-					wlc.Welcome(),
-				)
-
-				if err != nil {
-					log.Warn("failed to accept connection to encrypted group", "error", err.Error())
-					return
-				}
-
-				log.Info(
-					"accepted connection encrypted group",
-					"from", wlc.FromAddress().String(),
-					"group", groupAddress.String(),
-				)
-			},
-			// invoked when there is a message sent to an encrypted group we are subscribed to
+			OnWelcome: account.DefaultWelcomeAccept,
 			OnMessage: func(selfAccount *account.Account, msg *event.Message) {
 				switch event.ContentTypeOf(msg) {
 				case message.ContentTypeDiscoveryResponse:
-					log.Info(
-						"received response to discovery request",
-						"from", msg.FromAddress().String(),
-						"requestId", hex.EncodeToString(msg.ID()),
-					)
-
-					discoveryResponse, err := message.DecodeDiscoveryResponse(msg.Content())
-					if err != nil {
-						log.Warn("failed to decode discovery response", "error", err)
-						return
-					}
-
-					completer, ok := requests.LoadAndDelete(hex.EncodeToString(discoveryResponse.ResponseTo()))
-					if !ok {
-						log.Warn(
-							"received response to unknown request",
-							"requestId", hex.EncodeToString(msg.ID()),
-							"responseTo", hex.EncodeToString(discoveryResponse.ResponseTo()),
-						)
-						return
-					}
-
-					completer.(chan *signing.PublicKey) <- msg.FromAddress()
+					handleDiscoveryResponse(msg)
 				case message.ContentTypeCredentialVerificationResponse:
-					log.Info(
-						"received response to credential verification request",
-						"from", msg.FromAddress().String(),
-						"requestId", hex.EncodeToString(msg.ID()),
-					)
-
-					credentialVerificationResponse, err := message.DecodeCredentialVerificationResponse(msg.Content())
-					if err != nil {
-						log.Warn("failed to decode discovery response", "error", err)
-						return
-					}
-
-					completer, ok := requests.LoadAndDelete(hex.EncodeToString(credentialVerificationResponse.ResponseTo()))
-					if !ok {
-						log.Warn(
-							"received response to unknown request",
-							"requestId", hex.EncodeToString(msg.ID()),
-							"responseTo", hex.EncodeToString(credentialVerificationResponse.ResponseTo()),
-						)
-						return
-					}
-
-					completer.(chan *message.CredentialVerificationResponse) <- credentialVerificationResponse
+					handleCredentialVerificationResponse(msg)
 				}
 			},
 		},
 	}
 
-	log.Info("initializing self account")
-
-	// initialize and load the account
 	selfAccount, err := account.New(cfg)
 	if err != nil {
-		log.Fatal("failed to initialize account", "error", err)
+		log.Fatal(err)
 	}
-
-	// TODO : this will look slightly different in production.
-	// right now, we can just open an inbox to send and receive
-	// messages from it. In the future we will hide some of this
-	// and do proper linking with the application identity.
-	// NB: this does not need to happen every time we start the SDK,
-	// only once!
-	inboxAddress, err := selfAccount.InboxOpen()
-	if err != nil {
-		log.Fatal("failed to open account inbox", "error", err)
-	}
-
-	log.Info("initialized account success")
 
 	for {
-		// to determine which user we are interacting with, we can generate a
-		// discovery request and encode it to a qr code that your users can scan.
-		// as we may not have interacted with this user before, we need to prepare
-		// information they need to establish an encrypted group
+		expiry := time.Now().Add(time.Minute * 5)
 
-		// get a key package that the responder can use to create an encryped group
-		// for us, if there is not already an existing one.
-		keyPackage, err := selfAccount.ConnectionNegotiateOutOfBand(
-			inboxAddress,
-			time.Now().Add(time.Minute*5),
-		)
-
+		keyPackage, err := selfAccount.ConnectionNegotiateOutOfBand(selfAccount.InboxDefault(), expiry)
 		if err != nil {
-			log.Fatal("failed to generate key package", "error", err)
+			log.Fatal(err)
 		}
 
-		// build the key package into a discovery request
-		content, err := message.NewDiscoveryRequest().
-			KeyPackage(keyPackage).
-			Expires(time.Now().Add(time.Minute * 5)).
-			Finish()
-
+		content, err := message.NewDiscoveryRequest().KeyPackage(keyPackage).Expires(expiry).Finish()
 		if err != nil {
-			log.Fatal("failed to build discovery request", "error", err)
+			log.Fatal(err)
 		}
 
-		// create a channel to track the response from our qr code
-		discoveryCompleter := make(chan *signing.PublicKey, 1)
+		discoveryCompleter := make(chan *event.Message, 1)
+		requests.Store(hex.EncodeToString(content.ID()), discoveryCompleter)
 
-		requests.Store(
-			hex.EncodeToString(content.ID()),
-			discoveryCompleter,
-		)
-
-		// encode it as a QR code. This can be encoded as either an SVG
-		// for use in rendering on a web page, or Unicode, for encoding
-		// in text based environments like a terminal
-		qrCode, err := event.NewAnonymousMessage(content).
-			SetFlags(event.MessageFlagTargetSandbox).
-			EncodeToQR(event.QREncodingUnicode)
-
+		qrCode, err := event.NewAnonymousMessage(content).SetFlags(event.MessageFlagTargetSandbox).EncodeToQR(event.QREncodingUnicode)
 		if err != nil {
-			log.Fatal("failed to encode anonymous message", "error", err)
+			log.Fatal(err.Error())
 		}
-
-		log.Info("scan the qr code to complete the discovery request")
 
 		fmt.Println(string(qrCode))
 
-		log.Info(
-			"waiting for response to discovery request",
-			"requestId", hex.EncodeToString(content.ID()),
-		)
-
-		responderAddress := <-discoveryCompleter
+		discoveryResponse := <-discoveryCompleter
 
 		// Read the terms.pdf file
 		pdf := fpdf.New("P", "mm", "A4", "")
@@ -214,85 +78,69 @@ func main() {
 
 		err = pdf.Output(agreementBuf)
 		if err != nil {
-			log.Fatal("failed to build agreement pdf", "error", err.Error())
+			log.Fatal(err)
 		}
 
-		agreementTerms, err := object.New(
-			"application/pdf",
-			agreementBuf.Bytes(),
-		)
-
+		agreementTerms, err := object.New("application/pdf", agreementBuf.Bytes())
 		if err != nil {
-			log.Error("failed to encrypt object", "error", err)
+			log.Fatal(err)
 		}
 
-		err = selfAccount.ObjectUpload(
-			agreementTerms,
-			false,
-		)
-
+		err = selfAccount.ObjectUpload(agreementTerms, false)
 		if err != nil {
-			log.Error("failed to upload object", "error", err)
+			log.Fatal(err)
 		}
 
-		// create the body of the credential agreement
-		// when validating the agreement, any validator will be
-		// required to ensure they have been presented signed
-		// credentials for all parties
+		// create the body of the credential agreement when validating the agreement, any validator
+		// will be required to ensure they have been presented signed credentials for all parties
 		claims := map[string]interface{}{
 			"termsHash": hex.EncodeToString(agreementTerms.Hash()),
 			"parties": []map[string]interface{}{
 				{
 					"type": "signatory",
-					"id":   inboxAddress.String(),
+					"id":   discoveryResponse.FromAddress().String(),
 				},
 				{
 					"type": "signatory",
-					"id":   responderAddress.String(),
+					"id":   selfAccount.InboxDefault().String(),
 				},
 			},
 		}
 
-		// create a credential to serve as our agreement
-		// the subject of our credential will be ourselves,
-		// signifying our agreement to the terms.
-		// our counterparty will issue a credential in the
-		// same manner.
+		// create a credential to serve as our agreement the subject of our credential will be ourselves,
+		// signifying our agreement to the terms. our counterparty will issue a credential in the same manner.
 		unsignedAgreementCredential, err := credential.NewCredential().
 			CredentialType([]string{"VerifiableCredential", "AgreementCredential"}).
-			CredentialSubject(credential.AddressKey(inboxAddress)).
+			CredentialSubject(credential.AddressKey(selfAccount.InboxDefault())).
 			CredentialSubjectClaims(claims).
 			CredentialSubjectClaim("terms", hex.EncodeToString(agreementTerms.Id())).
-			Issuer(credential.AddressKey(inboxAddress)).
+			Issuer(credential.AddressKey(selfAccount.InboxDefault())).
 			ValidFrom(time.Now()).
-			SignWith(inboxAddress, time.Now()).
+			SignWith(selfAccount.InboxDefault(), time.Now()).
 			Finish()
 
 		if err != nil {
-			log.Fatal("failed to create credential", "error", err)
+			log.Fatal(err)
 		}
 
 		signedAgreementCredential, err := selfAccount.CredentialIssue(unsignedAgreementCredential)
 		if err != nil {
-			log.Fatal("failed to issue credential", "error", err)
+			log.Fatal(err)
 		}
 
 		unsignedAgreementPresentation, err := credential.NewPresentation().
 			PresentationType([]string{"VerifiablePresentation", "AgreementPresentation"}).
-			Holder(credential.AddressKey(inboxAddress)).
+			Holder(credential.AddressKey(selfAccount.InboxDefault())).
 			CredentialAdd(signedAgreementCredential).
 			Finish()
 
 		if err != nil {
-			log.Fatal("failed to create presentation", "error", err)
+			log.Fatal(err)
 		}
 
-		signedAgreementPresentation, err := selfAccount.PresentationIssue(
-			unsignedAgreementPresentation,
-		)
-
+		signedAgreementPresentation, err := selfAccount.PresentationIssue(unsignedAgreementPresentation)
 		if err != nil {
-			log.Fatal("failed to issue presentation", "error", err)
+			log.Fatal(err)
 		}
 
 		// create a new request and store a reference to it
@@ -304,50 +152,33 @@ func main() {
 			Finish()
 
 		if err != nil {
-			log.Fatal("failed to encode credential request message", "error", err)
+			log.Fatal(err)
 		}
 
 		verificationCompleter := make(chan *message.CredentialVerificationResponse, 1)
+		requests.Store(hex.EncodeToString(content.ID()), verificationCompleter)
 
-		requests.Store(
-			hex.EncodeToString(content.ID()),
-			verificationCompleter,
-		)
-
-		// send the presentation request to the responding user
-		err = selfAccount.MessageSend(responderAddress, content)
+		err = selfAccount.MessageSend(discoveryResponse.FromAddress(), content)
 		if err != nil {
-			log.Fatal("failed to send credential request message", "error", err)
+			log.Fatal(err)
 		}
 
-		response := <-verificationCompleter
+		verificationResponse := <-verificationCompleter
 
-		log.Info("Response received with status", "status", response.Status())
-		for _, c := range response.Credentials() {
+		for _, c := range verificationResponse.Credentials() {
 			err = c.Validate()
 			if err != nil {
-				log.Warn("failed to validate credential", "error", err)
-				continue
+				log.Fatal("failed to validate credential", "error", err)
 			}
-
-			/*
-				// check that the credential is not yet valid for use
-				if c.ValidFrom().After(time.Now()) {
-					log.Warn("credential is intended to be used in the future")
-					continue
-				}
-			*/
 
 			claims, err := c.CredentialSubjectClaims()
 			if err != nil {
-				log.Warn("failed to parse credential claims", "error", err)
-				continue
+				log.Fatal(err)
 			}
 
 			parties, ok := claims["parties"].([]interface{})
 			if !ok {
-				log.Warn("parties claim is not an array")
-				continue
+				log.Fatal("parties claim is not an array")
 			}
 
 			var isIssued, isSigner bool
@@ -355,8 +186,7 @@ func main() {
 			for _, subject := range parties {
 				subjectDetails, ok := subject.(map[string]interface{})
 				if !ok {
-					log.Warn("subject is not an object")
-					continue
+					log.Fatal("subject is not an object")
 				}
 
 				subjectType, ok := subjectDetails["type"].(string)
@@ -366,28 +196,59 @@ func main() {
 
 				subjectID, ok := subjectDetails["id"].(string)
 				if !ok {
-					log.Warn("subject id is not a string")
-					continue
+					log.Fatal("subject id is not a string")
 				}
 
 				// check if the agreement issuer (alice) is provided in the agreement
-				if subjectID == inboxAddress.String() {
+				if subjectID == selfAccount.InboxDefault().String() {
 					isIssued = true
 				}
 
 				// check if the responder is included as a signer in the agreement
-				if subjectID == responderAddress.String() {
+				if subjectID == discoveryResponse.FromAddress().String() {
 					isSigner = true
 				}
 			}
 
 			if isIssued && isSigner {
-				log.Info("Agreement is valid and signed by both parties")
+				log.Println("Agreement is valid and signed by both parties")
 				selfAccount.CredentialStore(c)
 			} else {
-				log.Warn("Agreement is not valid or not signed by both parties")
+				log.Fatal("Agreement is not valid or not signed by both parties")
 			}
 		}
 		os.Exit(1)
 	}
+}
+
+func handleDiscoveryResponse(msg *event.Message) {
+	discoveryResponse, err := message.DecodeDiscoveryResponse(msg.Content())
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	completer, ok := requests.LoadAndDelete(hex.EncodeToString(discoveryResponse.ResponseTo()))
+	if !ok {
+		log.Fatal("received response to an unknown discovery request")
+	}
+
+	completer.(chan *event.Message) <- msg
+}
+
+func handleCredentialVerificationResponse(msg *event.Message) {
+	credentialVerificationResponse, err := message.DecodeCredentialVerificationResponse(msg.Content())
+	if err != nil {
+		log.Fatal("failed to decode discovery response", "error", err)
+	}
+
+	completer, ok := requests.LoadAndDelete(hex.EncodeToString(credentialVerificationResponse.ResponseTo()))
+	if !ok {
+		log.Fatalf(
+			"received response to unknown request. requestId: %s responseTo: %s",
+			hex.EncodeToString(msg.ID()),
+			hex.EncodeToString(credentialVerificationResponse.ResponseTo()),
+		)
+	}
+
+	completer.(chan *message.CredentialVerificationResponse) <- credentialVerificationResponse
 }
--- examples/chat/main.go
+++ examples/chat/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"encoding/hex"
 	"fmt"
+	"log"
 	"sync"
 	"time"
 
-	"github.com/charmbracelet/log"
 	"github.com/joinself/self-go-sdk/account"
 	"github.com/joinself/self-go-sdk/event"
 	"github.com/joinself/self-go-sdk/message"
@@ -15,257 +15,117 @@ import (
 var requests sync.Map
 
 func main() {
-	// initialize an account that will be used to interact with self and other entities on
-	// the network. the account provides storage of all cryptographic key material, as well
-	// as credentials and all state used for e2e encrypted messaging with other entitites
 	cfg := &account.Config{
-		// provide a secure storage key that will  be used to encrypt your local account
-		// state. this should be replaced with a securely generated key!
-		StorageKey: make([]byte, 32),
-		// provide a storage path to the directory where your local account state will be
-		// stored
+		StorageKey:  []byte("my secure random key"),
 		StoragePath: "./storage",
-		// provide an environment to target [Develop, Sandbox]
 		Environment: account.TargetSandbox,
-		// provide the level of log granularity [Error, Warn, Info, Debug, Trace]
-		LogLevel: account.LogWarn,
-		// specify callbacks to handle events
 		Callbacks: account.Callbacks{
-			// invoked when the messaging socket connects
-			OnConnect: func(selfAccount *account.Account) {
-				log.Info("messaging socket connected")
-			},
-			// invoked when the messaging socket disconnects. if there is no error
-			OnDisconnect: func(selfAccount *account.Account, err error) {
-				if err != nil {
-					log.Warn("messaging socket disconnected", "error", err)
-				} else {
-					log.Info("messaging socket disconnected")
-				}
-			},
-			// invoked when there is an invite to a new encrypted group
-			OnWelcome: func(selfAccount *account.Account, wlc *event.Welcome) {
-				// a user has scanned the qr code and is attempting to create
-				// a new encrypted group with this account (if one does not exist).
-				// accept the invite to join the encrypted group created by the user.
-				groupAddres, err := selfAccount.ConnectionAccept(
-					wlc.ToAddress(),
-					wlc.Welcome(),
-				)
-
-				if err != nil {
-					log.Warn("failed to accept connection to encrypted group", "error", err.Error())
-					return
-				}
-
-				log.Info(
-					"accepted connection encrypted group",
-					"from", wlc.FromAddress().String(),
-					"group", groupAddres.String(),
-				)
-			},
-			// invoked when there is a message sent to an encrypted group we are subscribed to
+			OnWelcome: account.DefaultWelcomeAccept,
 			OnMessage: func(selfAccount *account.Account, msg *event.Message) {
 				switch event.ContentTypeOf(msg) {
 				case message.ContentTypeDiscoveryResponse:
-					log.Info(
-						"received response to discovery request",
-						"from", msg.FromAddress().String(),
-						"requestId", hex.EncodeToString(msg.ID()),
-					)
-
-					discoveryResponse, err := message.DecodeDiscoveryResponse(msg.Content())
-					if err != nil {
-						log.Warn("failed to decode discovery response", "error", err)
-						return
-					}
-
-					completer, ok := requests.LoadAndDelete(hex.EncodeToString(discoveryResponse.ResponseTo()))
-					if !ok {
-						log.Warn(
-							"received response to unknown request",
-							"requestId", hex.EncodeToString(msg.ID()),
-						)
-						return
-					}
-
-					completer.(chan *event.Message) <- msg
-
+					handleDiscoveryResponse(msg)
 				case message.ContentTypeIntroduction:
-					introduction, err := message.DecodeIntroduction(msg.Content())
-					if err != nil {
-						log.Warn("failed to decode introduction", "error", err)
-						return
-					}
-
-					tokens, err := introduction.Tokens()
-					if err != nil {
-						log.Warn("failed to decode introduction tokens", "error", err)
-						return
-					}
-
-					for _, token := range tokens {
-						err = selfAccount.TokenStore(
-							msg.FromAddress(),
-							msg.ToAddress(),
-							msg.ToAddress(),
-							token,
-						)
-
-						if err != nil {
-							log.Warn("failed to store introduction tokens", "error", err)
-							return
-						}
-					}
-
-					log.Info(
-						"received introduction",
-						"from", msg.FromAddress().String(),
-						"messageId", hex.EncodeToString(msg.ID()),
-						"tokens", len(tokens),
-					)
-
+					handleIntroduction(selfAccount, msg)
 				case message.ContentTypeChat:
-					chat, err := message.DecodeChat(msg.Content())
-					if err != nil {
-						log.Warn("failed to decode chat", "error", err)
-						return
-					}
-
-					log.Info(
-						"received chat message",
-						"from", msg.FromAddress().String(),
-						"messageId", hex.EncodeToString(msg.ID()),
-						"referencing", hex.EncodeToString(chat.Referencing()),
-						"message", chat.Message(),
-						"attachments", len(chat.Attachments()),
-					)
+					handleChat(msg)
 				}
 			},
 		},
 	}
 
-	log.Info("initializing self account")
-
-	// initialize and load the account
 	selfAccount, err := account.New(cfg)
 	if err != nil {
-		log.Fatal("failed to initialize account", "error", err)
+		log.Fatal(err.Error())
 	}
-
-	inboxAddress, err := selfAccount.InboxOpen()
-	if err != nil {
-		log.Fatal("failed to open account inbox", "error", err)
-	}
-
-	log.Info("initialized account success")
 
 	for {
-		// to determine which user we are interacting with, we can generate a
-		// discovery request and encode it to a qr code that your users can scan.
-		// as we may not have interacted with this user before, we need to prepare
-		// information they need to establish an encrypted group
-
-		// get a key package that the responder can use to create an encryped group
-		// for us, if there is not already an existing one.
-		keyPackage, err := selfAccount.ConnectionNegotiateOutOfBand(
-			inboxAddress,
-			time.Now().Add(time.Minute*5),
-		)
-
+		keyPackage, err := selfAccount.ConnectionNegotiateOutOfBand(selfAccount.InboxDefault(), time.Now().Add(time.Minute*5))
 		if err != nil {
-			log.Fatal("failed to generate key package", "error", err)
+			log.Fatal(err.Error())
 		}
 
-		// build the key package into a discovery request
-		content, err := message.NewDiscoveryRequest().
-			KeyPackage(keyPackage).
-			Expires(time.Now().Add(time.Minute * 5)).
-			Finish()
-
+		content, err := message.NewDiscoveryRequest().KeyPackage(keyPackage).Expires(time.Now().Add(time.Minute * 5)).Finish()
 		if err != nil {
-			log.Fatal("failed to build discovery request", "error", err)
+			log.Fatal(err.Error())
 		}
 
-		// create a channel to track the response from our qr code
 		completer := make(chan *event.Message, 1)
 
-		requests.Store(
-			hex.EncodeToString(content.ID()),
-			completer,
-		)
+		requests.Store(hex.EncodeToString(content.ID()), completer)
 
-		// encode it as a QR code. This can be encoded as either an SVG
-		// for use in rendering on a web page, or Unicode, for encoding
-		// in text based environments like a terminal
-		qrCode, err := event.NewAnonymousMessage(content).
-			SetFlags(event.MessageFlagTargetSandbox).
-			EncodeToQR(event.QREncodingUnicode)
-
+		qrCode, err := event.NewAnonymousMessage(content).SetFlags(event.MessageFlagTargetSandbox).EncodeToQR(event.QREncodingUnicode)
 		if err != nil {
-			log.Fatal("failed to encode anonymous message", "error", err)
+			log.Fatal(err.Error())
 		}
-
-		log.Info("scan the qr code to complete the discovery request")
 
 		fmt.Println(string(qrCode))
 
-		log.Info(
-			"waiting for response to discovery request",
-			"requestId", hex.EncodeToString(content.ID()),
-		)
-
 		response := <-completer
 
-		log.Info(
-			"received response to discovery request",
-			"requestId", hex.EncodeToString(content.ID()),
-		)
-
-		content, err = message.NewChat().
-			Message("Hello!").
-			Finish()
-
+		content, err = message.NewChat().Message("Hello!").Finish()
 		if err != nil {
-			log.Fatal("failed to encode chat message", "error", err)
+			log.Fatal(err.Error())
 		}
 
-		log.Info(
-			"sending message",
-			"toAddress", response.FromAddress().String(),
-		)
-
-		err = selfAccount.MessageSend(
-			response.FromAddress(),
-			content,
-		)
-
+		err = selfAccount.MessageSend(response.FromAddress(), content)
 		if err != nil {
-			log.Fatal("failed to send chat message", "error", err)
+			log.Fatal(err.Error())
 		}
-
-		log.Info(
-			"sent message",
-			"toAddress", response.FromAddress().String(),
-		)
 
 		summary, err := content.Summary()
 		if err != nil {
-			log.Fatal("failed to create chat summary", "error", err)
+			log.Fatal(err.Error())
 		}
 
-		err = selfAccount.NotificationSend(
-			response.FromAddress(),
-			summary,
-		)
-
+		err = selfAccount.NotificationSend(response.FromAddress(), summary)
 		if err != nil {
-			log.Fatal("failed to send chat notification", "error", err)
+			log.Fatal(err.Error())
 		}
-
-		log.Info(
-			"sent notification",
-			"toAddress", response.FromAddress().String(),
-		)
 	}
+}
+
+func handleDiscoveryResponse(msg *event.Message) {
+	discoveryResponse, err := message.DecodeDiscoveryResponse(msg.Content())
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	completer, ok := requests.LoadAndDelete(hex.EncodeToString(discoveryResponse.ResponseTo()))
+	if !ok {
+		log.Println("received response to an unknown discovery request")
+		return
+	}
+
+	completer.(chan *event.Message) <- msg
+}
+
+func handleIntroduction(selfAccount *account.Account, msg *event.Message) {
+	introduction, err := message.DecodeIntroduction(msg.Content())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tokens, err := introduction.Tokens()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, token := range tokens {
+		err = selfAccount.TokenStore(msg.FromAddress(), msg.ToAddress(), msg.ToAddress(), token)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func handleChat(msg *event.Message) {
+	chat, err := message.DecodeChat(msg.Content())
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	log.Println("received message:", chat.Message())
 }
