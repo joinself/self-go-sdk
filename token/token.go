3c097e8fd8f7dda96bc3445afeb0d2a0603b1483
Tom Bevan <tom@joinself.com>
2025-11-09 22:15:29 +0400

    token encoding

--- token/token.go
+++ token/token.go
@@ -8,7 +8,12 @@ package token
 #include <stdlib.h>
 */
 import "C"
-import "runtime"
+import (
+	"runtime"
+	"unsafe"
+
+	"github.com/joinself/self-go-sdk/status"
+)
 
 type Token struct {
 	ptr *C.self_token
@@ -30,4 +35,26 @@ func newToken(ptr *C.self_token) *Token {
 
 func tokenPtr(t *Token) *C.self_token {
 	return t.ptr
+}
+
+func encodeToken(t *Token) ([]byte, error) {
+	var buf *C.self_bytes_buffer
+
+	result := C.self_token_encode(
+		t.ptr,
+		&buf,
+	)
+
+	if result > 0 {
+		return nil, status.New(result)
+	}
+
+	encodedToken := C.GoBytes(
+		unsafe.Pointer(C.self_bytes_buffer_buf(buf)),
+		C.int(C.self_bytes_buffer_len(buf)),
+	)
+
+	C.self_bytes_buffer_destroy(buf)
+
+	return encodedToken, nil
 }
